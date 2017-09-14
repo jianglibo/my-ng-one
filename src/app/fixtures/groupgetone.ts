480693bd40ec49e03d6a64dced2848c52661a515
jianglibo <jianglibo@gmail.com>
2017-09-14 16:03:49 +0800

    add more fixtures

---
+++ src/app/fixtures/groupgetone.ts
@@ -0,0 +1,38 @@
+export const GROUOP_ONE = {
+    "data" : {
+      "id" : "294912",
+      "type" : "groups",
+      "attributes" : {
+        "openToAll" : true,
+        "createdAt" : 1499650663034,
+        "name" : "agroup",
+        "description" : "agroupdescription",
+        "thumbUrl" : "abc",
+        "dtoAction" : null,
+        "dtoApplyTo" : null
+      },
+      "relationships" : {
+        "creator" : {
+          "links" : {
+            "self" : "http://localhost/jsonapi/groups/294912/relationships/creator",
+            "related" : "http://localhost/jsonapi/groups/294912/creator"
+          }
+        },
+        "members" : {
+          "links" : {
+            "self" : "http://localhost/jsonapi/groups/294912/relationships/members",
+            "related" : "http://localhost/jsonapi/groups/294912/members"
+          }
+        },
+        "receivedPosts" : {
+          "links" : {
+            "self" : "http://localhost/jsonapi/groups/294912/relationships/receivedPosts",
+            "related" : "http://localhost/jsonapi/groups/294912/receivedPosts"
+          }
+        }
+      },
+      "links" : {
+        "self" : "http://localhost/jsonapi/groups/294912"
+      }
+    }
+  };
---
+++ src/app/fixtures/postsgetone.ts
@@ -0,0 +1,44 @@
+export const POST_BODY = {
+    "data" : {
+      "id" : "65537",
+      "type" : "posts",
+      "attributes" : {
+        "toAll" : true,
+        "createdAt" : 1499829564891,
+        "read" : true,
+        "title" : "title",
+        "content" : "content",
+        "dtoAction" : null,
+        "dtoApplyTo" : null
+      },
+      "relationships" : {
+        "sharedUsers" : {
+          "links" : {
+            "self" : "http://localhost/jsonapi/posts/65537/relationships/sharedUsers",
+            "related" : "http://localhost/jsonapi/posts/65537/sharedUsers"
+          }
+        },
+        "creator" : {
+          "links" : {
+            "self" : "http://localhost/jsonapi/posts/65537/relationships/creator",
+            "related" : "http://localhost/jsonapi/posts/65537/creator"
+          }
+        },
+        "sharedGroups" : {
+          "links" : {
+            "self" : "http://localhost/jsonapi/posts/65537/relationships/sharedGroups",
+            "related" : "http://localhost/jsonapi/posts/65537/sharedGroups"
+          }
+        },
+        "media" : {
+          "links" : {
+            "self" : "http://localhost/jsonapi/posts/65537/relationships/media",
+            "related" : "http://localhost/jsonapi/posts/65537/media"
+          }
+        }
+      },
+      "links" : {
+        "self" : "http://localhost/jsonapi/posts/65537"
+      }
+    }
+  };
